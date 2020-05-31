dc0a8c76e666362dda28dca306dbfda1c231a335
ngdotdennis <zimmerer.dennis98@gmail.com>
2020-05-31 22:31:25 +0200

    Pattern Prefix/Suffix table preparation

--- C KMP TI2/main.c
+++ C KMP TI2/main.c
@@ -9,10 +9,32 @@
 
 void KMP_Setup(char search_Pattern[], int length_Pattern, int pi_Table[])
 {
+	int pi_Counter = 0;
+	pi_Table[1] = 0;			//String with a length of 1 has no real suffix
 
+	for (int i = 1; i < length_Pattern; i++)
+	{
+		while ((pi_Counter > 0) && (search_Pattern[i] != search_Pattern[pi_Counter]))	//Check found matches for potential match 
+		{
+			pi_Counter = pi_Table[pi_Counter];
+		}
+		if (search_Pattern[i] == search_Pattern[pi_Counter])
+		{
+			pi_Counter += 1;	//Increase pi table index counter if there's a match
+		}
+		pi_Table[i + 1] = pi_Counter;
+	}
+
+	printf("\n%s:\t%2s\n", getName(search_Pattern), search_Pattern);
+	printf("PI-Table:	");
+	for (int i = 1; i <= length_Pattern; i++) //Ignore token
+	{
+		printf("%d", pi_Table[i]);
+	}
+	newline;
 }
 
-void KMP_Search(char search_pattern[], int length_Pattern, char search_String, int length_String, int pi_Table[])
+void KMP_Search(char search_pattern[], int length_Pattern, char search_String[], int length_String, int pi_Table[])
 {
 
 }
@@ -41,6 +63,8 @@ int main(void)
 
 	KMP_Setup(search_Pattern, length_Pattern, pi_Table);
 	KMP_Search(search_Pattern, length_Pattern, search_String, length_String, pi_Table);
+
+	free(pi_Table);
 
 	return 0;
 }
